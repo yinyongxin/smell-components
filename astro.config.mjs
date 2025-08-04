14a2728b1bd46c71180c279c1e80bea9f728bdd0
银永鑫 <1101278643@qq.com>
2025-08-04 21:41:48 +0800

    build(astro): 添加 Preact、SolidJS 和 Vue 集成
    - 在 astro.config.mjs 中引入 Preact、SolidJS 和 Vue 集成
    - 更新 package.json，添加相关依赖包
    - 配置 Preact 和 SolidJS 的包含路径

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import preact from "@astrojs/preact";
+
+import solidJs from "@astrojs/solid-js";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    preact({
+      include: ["**/preact/*"],
+    }),
+    solidJs({
+      include: ["**/solid/*"],
+    }),
+    vue(),
+  ],
+});
