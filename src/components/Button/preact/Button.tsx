f096b9cd09ec6f804fb06082161a1c586615267e
银永鑫 <1101278643@qq.com>
2025-08-06 01:16:32 +0800

    feat(Button): 添加主题切换功能并优化按钮组件
    - 在 Button 组件中添加 onClick 属性和 className 属性
    - 实现主题切换功能，点击按钮时切换主题
    - 更新 BaseLayout 组件，默认主题改为 light
    - 在首页添加主题切换按钮
    - 更新 global.css，添加 dark 主题样式

--- src/components/Button/preact/Button.tsx
+++ src/components/Button/preact/Button.tsx
@@ -1,30 +1,34 @@
-import type { FC } from "preact/compat"
-import type { ButtonProps, ButtonStatus } from "../types"
+import type { FC } from "preact/compat";
+import type { ButtonProps, ButtonStatus } from "../types";
+import { toggleTheme } from "@src/utils/theme";
 
 const Button: FC<ButtonProps> = (props) => {
-	const {
-		children,
-		status = 'default'
-	} = props
+  const { children, status = "default", className, ...rest } = props;
 
-	const statusClass: Record<ButtonStatus, string>[] = [
-		{
-			'danger': 'text-white',
-			'warning': 'text-white',
-			'success': 'text-white',
-			'default': 'text-white',
-		},
-		{
-			'danger': 'bg-danger',
-			'warning': 'bg-warning',
-			'success': 'bg-success',
-			'default': 'bg-primary',
-		}
-	]
+  const statusClass: Record<ButtonStatus, string>[] = [
+    {
+      danger: "text-",
+      warning: "text-",
+      success: "text-",
+      default: "text-",
+    },
+    {
+      danger: "bg-danger",
+      warning: "bg-warning",
+      success: "bg-success",
+      default: "bg-primary",
+    },
+  ];
 
-	return (
-		<button class={`h-10 min-w-10 px-4 rounded-md ${statusClass.map(item => item[status]).join(' ')}`}><>{children}</></button>
-	)
-}
+  return (
+    <button
+      onClick={() => toggleTheme()}
+      className={`h-10 min-w-10 px-4 rounded-md ${statusClass.map((item) => item[status]).join(" ")} ${className}`}
+      //   {...rest}
+    >
+      <>{children}</>
+    </button>
+  );
+};
 
-export default Button
+export default Button;
--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,8 +1,13 @@
-export type ButtonStatus  = 'default' | 'danger' | 'warning' | 'success' 
+import type { JSX } from "preact";
 
-export type ButtonVariant = 'primary' | 'secondary'  | 'outline' | 'text'
+export type ButtonStatus = "default" | "danger" | "warning" | "success";
+
+export type ButtonVariant = "primary" | "secondary" | "outline" | "text";
 
 export type ButtonProps = {
-	status?: ButtonStatus
-	variant?: ButtonVariant
-}
+  status?: ButtonStatus;
+  variant?: ButtonVariant;
+  onClick?: () => void;
+  className?: string;
+  class?: string;
+} & JSX.IntrinsicElements["button"];
