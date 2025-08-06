f3cbc8ccf337df3fe218565c0dcd1e701ac8ca05
银永鑫 <1101278643@qq.com>
2025-08-06 22:23:50 +0800

    style: 调整顶部导航栏样式
    - 修改导航栏文本颜色为 primary
    - 调整背景颜色为 primary/10
    - 调整内部 div 的背景颜色为 primary/20

--- src/components/preact/TopNavBar.tsx
+++ src/components/preact/TopNavBar.tsx
@@ -44,13 +44,13 @@ const TopNavBar = () => {
   }, []);
 
   return (
-    <nav className="flex justify-center">
-      <div className="relative bg-white/50 backdrop-blur rounded-full shadow">
+    <nav className="flex justify-center text-primary">
+      <div className="relative bg-primary/10 backdrop-blur rounded-full shadow">
         <div
           ref={checkRef}
           className="absolute top-2 bottom-2 left-2 transition-all"
         >
-          <div className="size-full bg-black/10 backdrop-blur rounded-full"></div>
+          <div className="size-full bg-primary/20 backdrop-blur rounded-full"></div>
         </div>
         <ul
           className="relative p-2 flex gap-2 "
