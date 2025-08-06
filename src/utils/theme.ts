78db16bcdf0ab23f88b3871c82a22ca15b6ad78e
银永鑫 <1101278643@qq.com>
2025-08-06 21:44:01 +0800

    build(deps): 添加 @radix-ui/react-dropdown-menu 依赖
    - 在 package.json 中新增 @radix-ui/react-dropdown-menu 依赖，版本为 2.1.15

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,9 +1,15 @@
 import type { ThemeType } from "@src/typings";
 
 export const toggleTheme = () => {
-  const currentTheme = document.documentElement.getAttribute("data-theme");
-  const newTheme = currentTheme === "light" ? "dark" : "light";
-  document.documentElement.setAttribute("data-theme", newTheme);
-  localStorage.setItem("theme", newTheme);
-  return newTheme as ThemeType;
+  // const currentTheme = document.documentElement.getAttribute("data-theme");
+  // const newTheme = currentTheme === "light" ? "dark" : "light";
+  // document.documentElement.setAttribute("data-theme", newTheme);
+  // localStorage.setItem("theme", newTheme);
+  // return newTheme as ThemeType;
+  // const isDarkMode = document.documentElement.classList.contains("dark")
+  //    const isDark =
+  //       theme === "dark" ||
+  //       (theme === "system" &&
+  //         window.matchMedia("(prefers-color-scheme: dark)").matches)
+  //     document.documentElement.classList[isDark ? "add" : "remove"]("dark")
 };
