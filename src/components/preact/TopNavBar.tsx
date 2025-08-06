e4ee1ad63ad540ae2a6dbe31e524cc4863f3fba1
银永鑫 <1101278643@qq.com>
2025-08-06 21:04:33 +0800

    build(dependencies): 添加 @lucide/astro 包并优化 TopNavBar 组件
    - 在 package.json 中添加 @lucide/astro 依赖
    - 更新 pnpm-lock.yaml 文件，包含新依赖的版本信息
    - 优化 TopNavBar 组件代码，提高可读性和性能

--- src/components/preact/TopNavBar.tsx
+++ src/components/preact/TopNavBar.tsx
@@ -1,76 +1,88 @@
-import { cn } from "@src/utils"
-import { useLayoutEffect, useEffect, useRef, useState } from "preact/hooks"
+import { cn } from "@src/utils";
+import { useLayoutEffect, useEffect, useRef, useState } from "preact/hooks";
 
 const TopNavBar = () => {
   const navList = [
     {
-      label: 'UI组件',
-      path: '/ui-components'
+      label: "UI组件",
+      path: "/ui-components",
     },
     {
-      label: '小组件',
-      path: '/smell-components'
+      label: "小组件",
+      path: "/smell-components",
     },
     {
-      label: '测试',
-      path: '/test-pages'
+      label: "测试",
+      path: "/test-pages",
     },
-  ]
+  ];
 
-  const [active, setActive] = useState(0)
-  const ref = useRef<HTMLUListElement>(null)
-  const checkRef = useRef<HTMLDivElement>(null)
+  const [active, setActive] = useState(0);
+  const ref = useRef<HTMLUListElement>(null);
+  const checkRef = useRef<HTMLDivElement>(null);
 
-  const setPosition =(index: number) => {
+  const setPosition = (index: number) => {
     if (!checkRef.current || !ref.current) {
-      return
+      return;
     }
-    const child = ref.current.children[index]
-    const left = [...ref.current.children].filter((fItem, fIndex) => {
-      return fIndex < index
-    }).map(mItem => {
-      return mItem.clientWidth + 8
-    }).reduce((a, b) => a + b, 0)
-    checkRef.current.style.width = child.clientWidth + 'px'
-    checkRef.current.style.height = child.clientHeight + 'px'
-    checkRef.current.style.left = left + 8 + 'px'
-  }
+    const child = ref.current.children[index];
+    const left = [...ref.current.children]
+      .filter((fItem, fIndex) => {
+        return fIndex < index;
+      })
+      .map((mItem) => {
+        return mItem.clientWidth + 8;
+      })
+      .reduce((a, b) => a + b, 0);
+    checkRef.current.style.width = child.clientWidth + "px";
+    checkRef.current.style.height = child.clientHeight + "px";
+    checkRef.current.style.left = left + 8 + "px";
+  };
 
   useLayoutEffect(() => {
-    setPosition(0)
-  }, [])
+    setPosition(0);
+  }, []);
 
   return (
     <nav className="flex justify-center">
       <div className="relative bg-white/50 backdrop-blur rounded-full shadow">
-        <div ref={checkRef} className="absolute top-2 bottom-2 left-2 transition-all">
+        <div
+          ref={checkRef}
+          className="absolute top-2 bottom-2 left-2 transition-all"
+        >
           <div className="size-full bg-black/10 backdrop-blur rounded-full"></div>
         </div>
-        <ul className="relative p-2 flex gap-2 " ref={ref} onMouseLeave={() => {
-         setPosition(active)
-        }}>
+        <ul
+          className="relative p-2 flex gap-2 "
+          ref={ref}
+          onMouseLeave={() => {
+            setPosition(active);
+          }}
+        >
           {navList.map((item, index) => {
             return (
               <li
-                class={cn("h-10 px-4 flex justify-center items-center cursor-pointer", {
-                  'font-bold': active === index
-                })}
+                class={cn(
+                  "h-10 px-4 flex justify-center items-center cursor-pointer",
+                  {
+                    "font-bold": active === index,
+                  },
+                )}
                 onMouseEnter={() => {
-                  setPosition(index)
+                  setPosition(index);
                 }}
                 onClick={() => {
-                  setActive(index)
+                  setActive(index);
                 }}
               >
                 {item.label}
               </li>
-            )
+            );
           })}
         </ul>
-
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default TopNavBar
+export default TopNavBar;
